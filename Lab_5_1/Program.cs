d98fed328102a5e99db41567dccf9090dab488c2
IgoninMihail <92269370+IgoninMihail@users.noreply.github.com>
2021-12-24 10:53:46 +0200

    Добавьте файлы проекта.

---
+++ Lab_5_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+            int k = 0;
+            while (str.IndexOf(".", k) != -1)
+            {
+                k = str.IndexOf(".", k);
+                str = str.Insert(k + 1, " ");
+                k += 1;
+            }
+            Console.WriteLine(str);
+
+
+        }
+    }
+}
